293d8a678e3ae0abb546eb29c91df4a91c970f6b
Foka tatsinkou Robert bright <fokrober@e1r9p4.1337.ma>
2019-04-07 00:32:44 +0100

    libft.h ft_putstr.c

--- srcs/ft_putstr.c
+++ srcs/ft_putstr.c
@@ -1,23 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fokrober <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/04/04 14:09:29 by fokrober          #+#    #+#             */
-/*   Updated: 2019/04/07 00:27:14 by fokrober         ###   ########.fr       */
+/*   Created: 2019/04/07 00:03:38 by fokrober          #+#    #+#             */
+/*   Updated: 2019/04/07 00:23:56 by fokrober         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef _LIBFT_H
-# define _LIBFT_H
-# include "libft.h"
-# include <unistd.h>
-# define MIN_INT -2147483648
-# define MAX_INT 2147483647
+#include "libft.h"
 
-void	ft_putchar(char c);
-void	ft_putnbr(int n);
-void	ft_putstr(char const *str);
-#endif
+void	ft_putstr(char const *str)
+{
+	int i;
+
+	i = -1;
+	while (str[++i])
+		ft_putchar(str[i]);
+}
