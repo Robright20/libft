0ec811e9770416b2a2b45f56af20a198ca9fbd0f
Foka tatsinkou Robert bright <fokrober@e1r9p4.1337.ma>
2019-04-06 22:35:59 +0100

    adding macro for int limits

--- header/libft.h
+++ header/libft.h
@@ -6,7 +6,7 @@
 /*   By: fokrober <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/04 14:09:29 by fokrober          #+#    #+#             */
-/*   Updated: 2019/04/06 06:35:27 by fokrober         ###   ########.fr       */
+/*   Updated: 2019/04/06 22:30:32 by fokrober         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define _LIBFT_H
 # include "libft.h"
 # include <unistd.h>
+# define MIN_INT -2147483648 
+# define MAX_INT 2147483647
 
 void	ft_putchar(char c);
 void	ft_putnbr(int n);
